15c3f2e44e0e9275ede400d35421fd52edf47c50
Karna Balaji <120701163+Karna-Balaji@users.noreply.github.com>
2022-12-27 08:43:38 +0530

    Create story Generator.py

---
+++ story Generator.py
@@ -0,0 +1,8 @@
+# The task is to generate a random story every time user runs the program.
+# Every time the user runs the program, we must produce a random tale. We'll first store the
+# portions of the tales in distinct lists, and then use the Random module to choose random
+# sections of the stories from those lists
+
+# Importing random Module
+import random 
+
