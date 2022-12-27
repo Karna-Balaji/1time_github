e0ab5c48594f1e48609c92f6272be516b41663bc
Karna Balaji <120701163+Karna-Balaji@users.noreply.github.com>
2022-12-27 08:49:50 +0530

    Update story Generator.py

--- story Generator.py
+++ story Generator.py
@@ -6,3 +6,22 @@
 # Importing random Module
 import random 
 
+#Defining list of Phrases that helps tp build a story
+a = ['About 1000 years ago, ', ' In the 100 BC, ', 'Once upon a time, ']
+b = [' there lived a king. ', ' there was a thief named William Turner. ', ' there lived a farmer. ', 'there lived a pirate named Jack Sparrow. ']
+c = [' One day ', ' One full-moon night ']
+d= [' he was passing by ', ' he was going for a walk in ','he was going for an adventure ']
+e = [' the mountains ', ' the garden ', 'an Island ']
+f = [' he saw a man ', ' he saw a solider ']
+g = [' who seemed to be old ', ' who seemed young ']
+h = [' searching for Gold. ', ' digging for gold. ','searching for a rare treasure ']
+i = ['The king ','The thief ','The Farmer ',' The Pirate ']
+j = ['wanted to kill ','wanted to steal the loot from ','wanted to help and get equal shares for helping ']
+k = ['The man ','The Young Lady ','The Pirate ']
+L = ['But ','And ']
+m = ['He Killed ','He stole from ',' He Helped ']
+n = ['him ']
+o = ['to get The rare treasure ','to get the Gold ']
+p = ['and ']
+q = ['went on their ways ','escaped from there ']
+
